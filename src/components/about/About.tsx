45ac74675087ef2052c9401f60b0e334fe37efee
notlim_dev <elmotigre6@gmail.com>
2026-03-27 20:44:15 -0500

    section-about: reorganize code and optimice useEffect

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -3,27 +3,23 @@ import { ModelGamerRoom } from "../models3D/Scene001";
 import { Suspense, useEffect, useRef } from "react";
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import { OrbitControls as ThreeOrbitControls } from "three-stdlib";
-import { labelsData } from "@data/labeldata";
-import LabelsHtml from "@components/about/LabelsHtml";
 import { AnimationModel } from "@components/animations/AnimationModel";
 import { useDivRefsStore } from "@store/store-sections";
+import { labelsData } from "@data/labeldata";
+import { sceneConfig } from "@data/scene-config";
+import LabelsHtml from "@components/about/LabelsHtml";
 
 const About = () => {
   const sectionHeadRef = useRef<HTMLDivElement>(null);
-  const controlsRef = useRef<ThreeOrbitControls | null>(null);
   const divrefheight = useRef<HTMLDivElement>(null);
+  const controlsRef = useRef<ThreeOrbitControls | null>(null);
   const setDivRef = useDivRefsStore((state) => state.setDivRef);
 
   useEffect(() => {
-    if (sectionHeadRef.current) {
-      setDivRef("About-Me", sectionHeadRef);
-    }
-  }, [sectionHeadRef, setDivRef]);
-  useEffect(() => {
-    if (divrefheight.current) {
-      setDivRef("divrefheight", divrefheight);
-    }
-  }, [divrefheight, setDivRef]);
+    if (sectionHeadRef.current) setDivRef("About-Me", sectionHeadRef);
+    if (divrefheight.current) setDivRef("divrefheight", divrefheight);
+  }, [setDivRef]);
+
   return (
     <div
       id="About-Me"
@@ -36,25 +32,32 @@ const About = () => {
       >
         <div className="w-full flex flex-row justify-center items-center">
           <h1
-            className="w-[90%] text-white text-4xl 
-          font-bold text-center border-b-2 border-t-2 border-dashed"
+            className="w-[90%] text-white text-4xl font-bold text-center
+                         border-b-2 border-t-2 border-dashed"
           >
             About
           </h1>
         </div>
+
+        {/* Canvas 3D */}
         <div className="w-full h-full">
           <Canvas>
-            <PerspectiveCamera makeDefault position={[0, 0, 2]} />
+            <PerspectiveCamera
+              makeDefault
+              position={sceneConfig.camera.position}
+            />
+
             <ambientLight
-              intensity={1.7}
-              color={"#9288FF"}
-              position={[0, 6, 5]}
+              intensity={sceneConfig.lights.ambient.intensity}
+              color={sceneConfig.lights.ambient.color}
+              position={sceneConfig.lights.ambient.position}
             />
             <directionalLight
-              intensity={3}
-              color={"#9288FF"}
-              position={[1, 6, 10]}
+              intensity={sceneConfig.lights.directional.intensity}
+              color={sceneConfig.lights.directional.color}
+              position={sceneConfig.lights.directional.position}
             />
+
             <Suspense fallback={null}>
               <AnimationModel controlsRef={controlsRef}>
                 <ModelGamerRoom
@@ -64,23 +67,15 @@ const About = () => {
                 />
               </AnimationModel>
             </Suspense>
-            <OrbitControls
-              ref={controlsRef}
-              enableZoom={false}
-              enablePan={false}
-              enableRotate={false}
-              minAzimuthAngle={-Math.PI / 12}
-              maxAzimuthAngle={Math.PI / 12}
-              minPolarAngle={Math.PI * 0.3}
-              maxPolarAngle={Math.PI * 0.35}
-            />
+
+            <OrbitControls ref={controlsRef} {...sceneConfig.orbitControls} />
           </Canvas>
         </div>
-        <div className="absolute top-0 left-0 w-full h-full overflow-hidden z-10 pointer-events-none">
-          {labelsData.map((label) => (
-            <LabelsHtml id={label.id} label={label} key={label.id} />
-          ))}
-        </div>
+      </div>
+      <div className="absolute top-0 left-0 w-full h-full overflow-hidden z-10 pointer-events-none">
+        {labelsData.map((label) => (
+          <LabelsHtml id={label.id} label={label} key={label.id} />
+        ))}
       </div>
     </div>
   );
