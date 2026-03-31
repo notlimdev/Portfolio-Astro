b384a0eb41bbe20fbb54b5b617f93d78b7b5318f
notlim_dev <elmotigre6@gmail.com>
2026-03-30 20:04:57 -0500

    section-experience: remove dead code

--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -1,70 +1,44 @@
-import { useEffect, useRef, useState } from "react";
+// src/components/experience/Experience.tsx
+import { useEffect, useRef } from "react";
 import { TimelineLogo } from "./elements/TimelineLogo";
 import { useDivRefsStore } from "@store/store-sections";
+import { experienceData } from "@data/experience";
 
 export default function Experience() {
   const sectionExperience = useRef<HTMLDivElement>(null);
   const setDivRef = useDivRefsStore((state) => state.setDivRef);
 
-  const [fechas] = useState([
-    "Febrero-2022",
-    "Julio-2022",
-    "Diciembre-2022",
-    "Mazro-2023",
-  ]);
-
-  // Función para convertir y ordenar las fechas
-  const getSortedDates = () => {
-    return fechas
-      .map((date) => {
-        const [month, year] = date.split("-");
-        return {
-          dateString: date,
-          dateObj: new Date(`${year}-${month}-01`),
-        };
-      })
-      .sort((a, b) => {
-        // Comparar primero por año y luego por mes
-        const yearA = a.dateObj.getFullYear();
-        const yearB = b.dateObj.getFullYear();
-        const monthA = a.dateObj.getMonth();
-        const monthB = b.dateObj.getMonth();
-
-        // Ordenar por año descendente primero, luego por mes descendente
-        return yearB - yearA || monthB - monthA;
-      });
-  };
-  const dates = getSortedDates();
-
   useEffect(() => {
     if (sectionExperience.current) {
       setDivRef("Experience", sectionExperience);
     }
-  }, [sectionExperience, setDivRef]);
-  return (
+  }, [setDivRef]);
 
-    <div ref={sectionExperience} id="testrefpinend"
-      className="flex flex-col justify-around items-center w-full h-[240vh] max-sm:h-[150vh]">
-      <div className="w-full flex flex-row justify-center items-center">
-        <h1 className="w-[90%] text-white text-4xl 
-          font-bold text-center border-b-2 border-t-2 border-dashed">Experience</h1>
+  return (
+    <div
+      ref={sectionExperience}
+      id="Experience"
+      className="gap-y-3.5 w-full min-h-screen flex flex-col justify-start items-center"
+    >
+      <div className=" w-3/4 flex flex-row justify-center items-center">
+        <h1 className="w-[90%] text-white text-4xl font-bold text-center border-b-2 border-t-2 border-dashed">
+          Experience
+        </h1>
       </div>
-      <div id="Experience" className="relative w-full h-full">
-        <div>
-          <div
-            className="absolute max-sm:h-[89%] h-[90%] w-full max-sm:top-0
-            sm:top-28 flex flex-row justify-center items-center 
-            max-sm:justify-end max-sm:p-10"
-          >
-            <div className={`w-1 h-full border-2 border-slate-600 `}></div>
-          </div>
-          <div className="relative w-full h-full p-2 flex flex-col justify-normal items-center max-sm:justify-normal max-sm:gap-4">
-            {dates.map((date, index) => (
-              <TimelineLogo key={index} date={date} index={index} />
-            ))}
-          </div>
+
+      <div className="w-full flex-1 relative flex flex-col justify-center items-center">
+        <div className=" absolute max-lg:right-20 max-sm:right-5 w-1 h-[95%]  border-2 border-slate-600 z-0"></div>
+
+        <div className="py-2 gap-2 w-full flex-1 relative top-0 left-0 flex flex-col justify-center items-center">
+          {experienceData.map((experience, index) => (
+            <TimelineLogo
+              key={experience.id}
+              index={index}
+              experience={experience}
+            />
+          ))}
         </div>
-      </div >
-    </div >
+      </div>
+    </div>
   );
 }
