a3f53757dc1c4190f5b6ca181413d0c92508b81c
notlim_dev <elmotigre6@gmail.com>
2026-04-06 18:37:29 -0500

    banner: update and styles description

--- src/components/head/elementsHeader/Presentation.tsx
+++ src/components/head/elementsHeader/Presentation.tsx
@@ -2,23 +2,29 @@ import { personalInfo } from "@data/personal-info";
 
 export default function Presentation() {
   return (
-    <div className="flex w-[70%] h-3/4 absolute top-20 sm:top-12 lg:top-24">
+    <div className="flex max-sm:w-full w-[70%] h-3/4 absolute max-sm:top-8  top-20 sm:top-12 lg:top-24">
       <div
         className="basis-[95%] text-white relative top-20 space-y-10 p-10
-                      max-sm:top-5 max-sm:space-y-1 max-sm:p-4
+                      max-sm:top-2
+                      max-sm:p-4
+                      max-sm:space-y-0
                       sm:top-0
+                      sm:space-y-5
+                      sm:p-8
                       md:top-10
                       2xl:p-16"
       >
         <h1
           className="text-7xl
                        max-sm:text-lg
+                       max-sm:p-2
                        sm:text-2xl
+                       sm:p-2
                        md:text-4xl
                        lg:text-5xl
                        2xl:text-7xl"
         >
-          Hello 👋 my name <br />
+          Hola 👋 soy <br />
           <span
             className="font-extrabold bg-clip-text text-transparent
                            bg-linear-to-r from-green-500 to-yellow-500
@@ -29,15 +35,17 @@ export default function Presentation() {
                            lg:text-6xl
                            2xl:text-8xl"
           >
-            is {personalInfo.name}
+            {personalInfo.name}
           </span>
         </h1>
 
         {/* Mobile: descripción corta / Desktop: descripción larga */}
-        <p className="lg:text-xl max-sm:text-sm block sm:hidden">
+        <p className="lg:text-xl max-sm:border-b-2 max-sm:border-b-indigo-300 max-sm:rounded-3xl max-sm:p-2 max-sm:text-sm block sm:hidden">
           {personalInfo.shortBio}
         </p>
-        <p className="lg:text-xl hidden sm:block">{personalInfo.longBio}</p>
+        <p className="lg:text-xl hidden sm:block border-b-2 border-b-indigo-300 rounded-3xl p-2">
+          {personalInfo.longBio}
+        </p>
       </div>
     </div>
   );
